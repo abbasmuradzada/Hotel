affe9e3bf1a30a360fc21afaccf1070b4179b51b
unknown <abbasmurad11>
2020-04-29 10:45:05 +0400

    first v

---
+++ main.js
@@ -0,0 +1,16 @@
+function rooms(){
+    window.location.href="rooms.html";
+}
+function restaurant(){
+    window.location.href="restaurant.html";
+}
+function about(){
+    window.location.href="about.html";
+}
+function contact(){
+    window.location.href="contact.html";
+}
+
+function home(){
+    window.location.href="index.html";
+}
